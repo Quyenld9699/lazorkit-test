e399ef3b7d6358e3df885cb0e1e44fb3839ee335
Le Dinh Quyen <quyenld9699@gmail.com>
2025-09-09 11:22:46 +0700

    update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import LazorkitAppProvider from "@/components/LazorkitProvider/LazorkitProvider"
 
 export default function Home() {
     return (
-        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <div className="font-sans  min-h-screen p-8 pb-20 gap-16 sm:p-20">
             <LazorkitAppProvider>
                 <WalletDemo2></WalletDemo2>
             </LazorkitAppProvider>
