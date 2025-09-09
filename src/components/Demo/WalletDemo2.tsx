f73c4870f74d6214d25f456c2e5f6a7ffa451596
Le Dinh Quyen <quyenld9699@gmail.com>
2025-09-09 08:52:39 +0700

    update show error

--- src/components/Demo/WalletDemo2.tsx
+++ src/components/Demo/WalletDemo2.tsx
@@ -3,7 +3,7 @@ import { useWallet } from "@lazorkit/wallet";
 import React from "react";
 
 export default function WalletDemo2() {
-    const { connect, disconnect, isConnected, smartWalletPubkey } = useWallet();
+    const { connect, disconnect, isConnected, smartWalletPubkey, error } = useWallet();
 
     return (
         <div>
@@ -14,11 +14,17 @@ export default function WalletDemo2() {
                 </button>
             ) : (
                 <div>
-                    <p>Connected: {smartWalletPubkey?.toString().slice(0, 8)}...</p>
+                    <p>Connected: {smartWalletPubkey?.toString()}</p>
                     <button style={{ padding: "10px 16px", background: "red", borderRadius: "10px", color: "white", cursor: "pointer" }} onClick={disconnect}>
                         Disconnect
                     </button>
                 </div>
+            )}
+
+            {error && (
+                <p style={{ color: "red", marginTop: "20px" }}>
+                    Error: {error.message} | {error.stack}
+                </p>
             )}
         </div>
     );
